5478ff0e00970e7935df467a7dd057b83eac3b5a
daehwan2 <dhdh0482@naver.com>
2022-05-11 23:17:44 +0900

    Chore: react-redux 설치 및 세팅

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
 import App from "./components/App";
 import "./index.css";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
