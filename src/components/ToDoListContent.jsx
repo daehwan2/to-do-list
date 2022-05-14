e79c0ee71371230a96485d97bdb356f0bf3c9a32
daehwan2 <dhdh0482@naver.com>
2022-05-14 15:42:10 +0900

    Feat: contentList 임시 display

--- src/components/ToDoListContent.jsx
+++ src/components/ToDoListContent.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const ToDoListContent = () => {
-  return <div>ToDoListContent</div>;
+  const contentList = useSelector((state) => state.content);
+  return (
+    <ol className="w-[100%] h-[100%] pt-[40px] pb-[45px] px-3 flex flex-col-reverse overflow-y-auto">
+      {Object.keys(contentList)
+        .map((i) => contentList[i])
+        .map((item, i) => (
+          <li
+            key={i}
+            className="flex items-center justify-start bg-white mb-2 p-1 rounded-[4px]">
+            <input type="checkbox" className="mr-1 shrink-0" name="" id="" />
+            <div className="grow">{item.text}</div>
+            <button type="button" className="shrink-0">
+              삭제
+            </button>
+          </li>
+        ))}
+    </ol>
+  );
 };
 
 export default ToDoListContent;
