b7f6c2a93a9c52512816d1c7caec637ec165103a
daehwan2 <dhdh0482@naver.com>
2022-05-16 00:25:16 +0900

    Feat: checkbox, delete button 기능 구현

--- src/components/ToDoListContent.jsx
+++ src/components/ToDoListContent.jsx
@@ -1,8 +1,21 @@
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { dbService } from "../fbase";
 
 const ToDoListContent = () => {
   const contentList = useSelector((state) => state.content);
+  const email = useSelector((state) => state.user.email);
+  const onCheck = async (e, id) => {
+    console.log(e.target.checked);
+    const targetItemRef = doc(dbService, email, id);
+    await updateDoc(targetItemRef, {
+      check: e.target.checked,
+    });
+  };
+  const onDeleteButton = async (e, id) => {
+    deleteDoc(doc(dbService, email, id));
+  };
   return (
     <ol className="w-[100%] h-[100%] pt-[40px] pb-[45px] px-3 flex flex-col-reverse overflow-y-auto">
       {Object.keys(contentList)
@@ -11,9 +24,19 @@ const ToDoListContent = () => {
           <li
             key={i}
             className="flex items-center justify-start bg-white mb-2 p-1 rounded-[4px]">
-            <input type="checkbox" className="mr-1 shrink-0" name="" id="" />
+            <input
+              type="checkbox"
+              className="mr-1 shrink-0"
+              name=""
+              id=""
+              onChange={(e) => onCheck(e, contentList[i].id)}
+              checked={contentList[i].check}
+            />
             <div className="grow">{item.text}</div>
-            <button type="button" className="shrink-0">
+            <button
+              type="button"
+              className="shrink-0"
+              onClick={(e) => onDeleteButton(e, contentList[i].id)}>
               삭제
             </button>
           </li>
