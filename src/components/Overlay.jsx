cefaf70e89b98c0abe65f2cda0fe0093a9f16b7d
daehwan2 <dhdh0482@naver.com>
2022-05-17 19:03:15 +0900

    Feat: Overlay, Sidebar 작업중

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Overlay = () => {
+  return (
+    <div className="fixed top-0 left-0 z-20 w-[100%] h-[100vh] invisible bg-[rgba(183,201,235,0.5) opacity-0 duration-300 ease-in-out]"></div>
+  );
+};
+
+export default Overlay;
