a2e189cdd8e68369c0591a985b67fdcd0cac6cfe
daehwan2 <dhdh0482@naver.com>
2022-05-19 22:19:40 +0900

    Feat: sidebar 활성화 위하여 overlay 생성 및 dispatch이용한 sidebar action 사용

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { HiMenu } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { SidebarClicked } from "../_actions/sidebar_action";
 
 const MenuButton = () => {
+  const dispatch = useDispatch();
+  const onClickMenuButton = () => {
+    dispatch(SidebarClicked());
+  };
   return (
-    <button type="button" className="p-2">
+    <button type="button" className="p-2" onClick={onClickMenuButton}>
       <HiMenu className="text-[24px]" />
     </button>
   );
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { OverlayClicked } from "../_actions/overlay_action";
 const Overlay = () => {
+  const isClicked = useSelector((state) => state.sidebar.isClicked);
+  const dispatch = useDispatch();
+  const onClickOverlay = () => {
+    dispatch(OverlayClicked());
+  };
   return (
-    <div className="fixed top-0 left-0 z-20 w-[100%] h-[100vh] invisible bg-[rgba(183,201,235,0.5) opacity-0 duration-300 ease-in-out]"></div>
+    <div
+      className={`fixed top-0 left-0 z-20 w-[100%] h-[100vh] bg-[rgba(183,201,235,0.5)] transition-all duration-300 ease-in-out${
+        isClicked ? "" : " invisible opacity-0"
+      }`}
+      onClick={onClickOverlay}></div>
+    //active시 visible, opacity-1
   );
 };
 
