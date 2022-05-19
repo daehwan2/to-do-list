208d5178936f34302fbf2827e3e76e403f15778c
daehwan2 <dhdh0482@naver.com>
2022-05-19 22:56:14 +0900

    Feat: Progress-bar 퍼센트 계산

--- src/components/SidebarProgressBar.jsx
+++ src/components/SidebarProgressBar.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 const SidebarProgressBar = () => {
+  const contentObjects = useSelector((state) => state.content);
+  const [percent, setPercent] = useState(0);
+  useEffect(() => {
+    calculateProgress();
+    console.log(contentObjects);
+  }, [contentObjects]);
+  const calculateProgress = () => {
+    const contentList = Object.keys(contentObjects).map(
+      (i) => contentObjects[i]
+    );
+    const length = contentList.length;
+    console.log("ll:", contentList.length);
+    let successCount = 0;
+    contentList.forEach((item) => {
+      if (item.check) {
+        successCount++;
+      }
+    });
+    console.log("성공:", successCount);
+    setPercent(((successCount / length) * 100).toFixed(2));
+    console.log(percent);
+  };
   return (
     <div className="flex flex-col justify-center items-center mb-10">
       <h2>일정 진행률</h2>
       <div className="w-[180px] h-[180px] bg-[#D9D9D9] rounded-full flex justify-center items-center">
         <div className="w-[150px] h-[150px] bg-[white] rounded-full flex justify-center items-center">
-          <strong>20%</strong>
+          <strong>{percent}%</strong>
         </div>
       </div>
     </div>
