afc397725861b1dc5f90bee72a88346495c32454
daehwan2 <dhdh0482@naver.com>
2022-05-19 22:37:22 +0900

    Feat: Logout 기능 추가

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import SidebarProgressBar from "./SidebarProgressBar";
+import { signOut } from "firebase/auth";
+import { authService } from "../fbase";
 const Sidebar = () => {
   const isClicked = useSelector((state) => state.sidebar.isClicked);
   const email = useSelector((state) => state.user.email);
+  const onLogout = () => {
+    signOut(authService);
+  };
   return (
     <div
       className={`w-[240px] p-5 fixed top-0 right-0 z-40 h-[100vh] bg-white transition-all duration-200 ease-in-out flex flex-col ${
@@ -21,7 +26,8 @@ const Sidebar = () => {
         </button>
         <button
           type="button"
-          className="p-2 bg-[#97BBFF] text-white rounded-lg">
+          className="p-2 bg-[#97BBFF] text-white rounded-lg"
+          onClick={onLogout}>
           <strong>로그아웃</strong>
         </button>
       </footer>
