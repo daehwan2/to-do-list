561174c2d20e9cce8c30cc14ca6691c2d1d1bee0
daehwan2 <dhdh0482@naver.com>
2022-05-21 15:09:08 +0900

    Feat: 이미지 업로드 구현

--- src/components/ToDoListContent.jsx
+++ src/components/ToDoListContent.jsx
@@ -1,7 +1,8 @@
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { dbService } from "../fbase";
+import { dbService, storageService } from "../fbase";
 
 const ToDoListContent = () => {
   const contentList = useSelector((state) => state.content);
@@ -13,32 +14,46 @@ const ToDoListContent = () => {
       check: e.target.checked,
     });
   };
-  const onDeleteButton = async (e, id) => {
-    deleteDoc(doc(dbService, email, id));
+  const onDeleteButton = async (e, id, img) => {
+    await deleteDoc(doc(dbService, email, id));
+    if (img !== "") await deleteObject(ref(storageService, img));
   };
   return (
     <ol className="w-[100%] h-[100%] pt-[40px] pb-[45px] px-3 flex flex-col-reverse overflow-y-auto">
       {Object.keys(contentList)
         .map((i) => contentList[i])
         .map((item, i) => (
-          <li
-            key={i}
-            className="flex items-center justify-start bg-white mb-2 p-1 rounded-[4px]">
-            <input
-              type="checkbox"
-              className="mr-1 shrink-0"
-              name=""
-              id=""
-              onChange={(e) => onCheck(e, contentList[i].id)}
-              checked={contentList[i].check}
-            />
-            <div className="grow">{item.text}</div>
-            <button
-              type="button"
-              className="shrink-0"
-              onClick={(e) => onDeleteButton(e, contentList[i].id)}>
-              삭제
-            </button>
+          <li key={i} className="bg-white mb-2 p-1 rounded-[4px]">
+            {item.image !== "" ? (
+              <img
+                src={item.image}
+                alt=""
+                className="w-[100%] h-[250px] object-contain"
+              />
+            ) : (
+              <></>
+            )}
+
+            <div className="flex items-center justify-start ">
+              <input
+                type="checkbox"
+                className="mr-1 shrink-0"
+                name=""
+                id=""
+                onChange={(e) => onCheck(e, contentList[i].id)}
+                checked={contentList[i].check}
+              />
+
+              <div className="grow">{item.text}</div>
+              <button
+                type="button"
+                className="shrink-0"
+                onClick={(e) =>
+                  onDeleteButton(e, contentList[i].id, contentList[i].image)
+                }>
+                삭제
+              </button>
+            </div>
           </li>
         ))}
     </ol>
--- src/components/ToDoListInputForm.jsx
+++ src/components/ToDoListInputForm.jsx
@@ -1,33 +1,95 @@
 import React, { useState } from "react";
 import { BsPlusSquare } from "react-icons/bs";
 import { collection, addDoc, setDoc, doc } from "firebase/firestore";
-import { dbService } from "../fbase";
+import { dbService, storageService } from "../fbase";
 import { useSelector } from "react-redux";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 
 const ToDoListInputForm = () => {
   const email = useSelector((state) => state.user.email);
   const [text, setText] = useState("");
-
+  const [imageUrl, setImageUrl] = useState("");
   const textChange = (e) => {
     setText(e.target.value);
   };
 
   const addListItem = async (e) => {
     e.preventDefault();
-    await addDoc(collection(dbService, email), {
-      check: false,
-      text,
-      createdAt: Date.now(),
-    });
+    try {
+      let tempImage = "";
+      if (imageUrl !== "") {
+        const response = await uploadString(
+          ref(storageService, email + "/" + uuidv4()),
+          imageUrl,
+          "data_url"
+        );
+        tempImage = await getDownloadURL(response.ref);
+      }
+      await addDoc(collection(dbService, email), {
+        check: false,
+        text,
+        image: tempImage,
+        createdAt: Date.now(),
+      });
+    } catch (err) {
+      console.log(err);
+    }
     setText("");
+
+    setImageUrl("");
+  };
+
+  const onImageSelect = (e) => {
+    console.log(e.target.files);
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = (finishedEvnet) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvnet;
+      console.log(result);
+      setImageUrl(result);
+    };
+  };
+
+  const closePreview = () => {
+    setImageUrl("");
   };
   return (
     <form
       className="bg-white flex items-center w-[100%] h-[45px] fixed bottom-0"
       onSubmit={addListItem}>
-      <button type="button" className="p-2">
+      <input
+        type="file"
+        name=""
+        id="img"
+        className="hidden"
+        onChange={onImageSelect}
+      />
+      <label htmlFor="img" className="p-2 cursor-pointer">
         <BsPlusSquare className="text-[24px]" />
-      </button>
+      </label>
+
+      <div
+        className={`absolute top-[-300px] left-[50%] translate-x-[-50%] w-[100%] h-[300px] bg-slate-400 opacity-80 ${
+          imageUrl === "" ? "hidden" : ""
+        }`}>
+        <img
+          src={imageUrl}
+          alt=""
+          className="object-contain w-[100%] h-[300px]"
+        />
+        <button
+          type="button"
+          className="absolute top-0 right-0 p-1"
+          onClick={closePreview}>
+          <IoMdCloseCircleOutline className="text-[30px] text-[red]" />
+        </button>
+      </div>
+
       <input
         type="text"
         placeholder="내용 입력"
