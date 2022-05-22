6d6603d1a12b1b532f5fc0d7d5da00b40396c86f
daehwan2 <dhdh0482@naver.com>
2022-05-22 14:55:25 +0900

    Feat: 업로드중 display

--- src/components/ToDoListInputForm.jsx
+++ src/components/ToDoListInputForm.jsx
@@ -11,6 +11,7 @@ const ToDoListInputForm = () => {
   const email = useSelector((state) => state.user.email);
   const [text, setText] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [isUpload, setIsUpload] = useState(false);
   const textChange = (e) => {
     setText(e.target.value);
   };
@@ -18,6 +19,7 @@ const ToDoListInputForm = () => {
   const addListItem = async (e) => {
     e.preventDefault();
     try {
+      setIsUpload(true);
       let tempImage = "";
       if (imageUrl !== "") {
         const response = await uploadString(
@@ -33,8 +35,11 @@ const ToDoListInputForm = () => {
         image: tempImage,
         createdAt: Date.now(),
       });
+      setIsUpload(false);
     } catch (err) {
       console.log(err);
+      alert("업로드 실패");
+      setIsUpload(false);
     }
     setText("");
 
@@ -59,50 +64,60 @@ const ToDoListInputForm = () => {
     setImageUrl("");
   };
   return (
-    <form
-      className="bg-white flex items-center w-[100%] h-[45px] fixed bottom-0"
-      onSubmit={addListItem}>
-      <input
-        type="file"
-        name=""
-        id="img"
-        className="hidden"
-        onChange={onImageSelect}
-      />
-      <label htmlFor="img" className="p-2 cursor-pointer">
-        <BsPlusSquare className="text-[24px]" />
-      </label>
+    <>
+      <form
+        className="bg-white flex items-center w-[100%] h-[45px] fixed bottom-0"
+        onSubmit={addListItem}>
+        <input
+          type="file"
+          name=""
+          id="img"
+          className="hidden"
+          onChange={onImageSelect}
+        />
+        <label htmlFor="img" className="p-2 cursor-pointer">
+          <BsPlusSquare className="text-[24px]" />
+        </label>
 
-      <div
-        className={`absolute top-[-300px] left-[50%] translate-x-[-50%] w-[100%] h-[300px] bg-slate-400 opacity-80 ${
-          imageUrl === "" ? "hidden" : ""
-        }`}>
-        <img
-          src={imageUrl}
-          alt=""
-          className="object-contain w-[100%] h-[300px]"
+        <div
+          className={`absolute top-[-300px] left-[50%] translate-x-[-50%] w-[100%] h-[300px] bg-slate-400 opacity-80 ${
+            imageUrl === "" ? "hidden" : ""
+          }`}>
+          <img
+            src={imageUrl}
+            alt=""
+            className="object-contain w-[100%] h-[300px]"
+          />
+          <button
+            type="button"
+            className="absolute top-0 right-0 p-1"
+            onClick={closePreview}>
+            <IoMdCloseCircleOutline className="text-[30px] text-[red]" />
+          </button>
+        </div>
+
+        <input
+          type="text"
+          placeholder="내용 입력"
+          className="grow"
+          onChange={textChange}
+          value={text}
         />
         <button
-          type="button"
-          className="absolute top-0 right-0 p-1"
-          onClick={closePreview}>
-          <IoMdCloseCircleOutline className="text-[30px] text-[red]" />
+          type="submit"
+          className="border-[#3F4756] border-[2px] rounded-[4px] px-2 mx-2">
+          입력
         </button>
-      </div>
+      </form>
 
-      <input
-        type="text"
-        placeholder="내용 입력"
-        className="grow"
-        onChange={textChange}
-        value={text}
-      />
-      <button
-        type="submit"
-        className="border-[#3F4756] border-[2px] rounded-[4px] px-2 mx-2">
-        입력
-      </button>
-    </form>
+      {isUpload ? (
+        <div className="bg-[rgba(0,0,0,0.8)] text-white text-[24px] w-[300px] h-[100px] rounded-lg absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-center">
+          <strong>업로드 중</strong>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
